657f69a0d585b388d5f2c348497a647d0205f9a6
Sn1ckey <82238466+Sn1ckey@users.noreply.github.com>
2021-12-11 19:53:27 +0200

    add hashcodes to colors

--- src/main/java/spd/trello/domain/enumerations/Color.java
+++ src/main/java/spd/trello/domain/enumerations/Color.java
@@ -1,12 +1,19 @@
 package spd.trello.domain.enumerations;
 
 public enum Color {
-    RED,
-    GREEN,
-    YELLOW,
-    BLACK,
-    NO_COLOR,
-    BLUE,
-    VIOLET,
-    ORANGE
+    NULL,
+    RED("#FF0000"),
+    GREEN("#008000"),
+    YELLOW("#FFFF00"),
+    BLACK("#000000"),
+    BLUE("#0000FF"),
+    VIOLET("#EE82EE"),
+    ORANGE("#FFA500");
+
+    Color() {
+    }
+
+    Color(String hash){
+    }
+
 }
