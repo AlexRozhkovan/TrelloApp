bdc3391299adc2a1b327121c54eb9744d99e5f8b
Sn1ckey <82238466+Sn1ckey@users.noreply.github.com>
2021-12-11 20:28:16 +0200

    add services for class Board and Card with methods create, print and update add Main Class

---
+++ src/main/java/spd/trello/service/BoardService.java
@@ -0,0 +1,40 @@
+package spd.trello.service;
+
+import spd.trello.domain.Board;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BoardService extends AbstractService<Board> {
+    static List<Board> storage = new ArrayList();
+
+    @Override
+    public Board create() {
+        Board board = new Board();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = sc.nextLine();
+        board.setName(name);
+        storage.add(board);
+        return board;
+    }
+
+    @Override
+    public void print(Board board) {
+        System.out.println(board);
+    }
+
+    @Override
+    public void update(int index, Board board) {
+        Board board1 = storage.get(index);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String newName = sc.nextLine();
+        board1.setName(newName);
+        board1.setUpdatedDate(LocalDateTime.now());
+
+
+    }
+}
