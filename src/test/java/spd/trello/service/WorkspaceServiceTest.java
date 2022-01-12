a0442195ce598264f575ff4d606ffa570219c4a4
orozhkovan <orozhkovan@eva.ua>
2022-01-12 16:32:45 +0200

    srry, can't do tests. Something go wrong

--- src/test/java/spd/trello/service/WorkspaceServiceTest.java
+++ src/test/java/spd/trello/service/WorkspaceServiceTest.java
@@ -6,6 +6,9 @@ import spd.trello.domain.Workspace;
 import spd.trello.domain.enumerations.WorkspaceVisibility;
 import spd.trello.repository.WorkspaceRepository;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 class WorkspaceServiceTest extends BaseTest {
 
     private final WorkspaceService service;
@@ -17,13 +20,13 @@ class WorkspaceServiceTest extends BaseTest {
 
     @Test
     void create() {
-        Workspace workspace = service.create("user", "Testname" , "TestDesc", WorkspaceVisibility.PUBLIC);
+        Workspace workspace = service.create("user", "Testname", "TestDesc", WorkspaceVisibility.PUBLIC);
         Assertions.assertNotNull(workspace);
         Assertions.assertAll(
-                ()->Assertions.assertEquals("user", workspace.getCreatedBy()),
-                ()->Assertions.assertEquals("Testname", workspace.getName()),
-                ()->Assertions.assertEquals("TestDesc", workspace.getDescription()),
-                ()->Assertions.assertEquals(WorkspaceVisibility.PUBLIC, workspace.getVisibility())
+                () -> Assertions.assertEquals("user", workspace.getCreatedBy()),
+                () -> Assertions.assertEquals("Testname", workspace.getName()),
+                () -> Assertions.assertEquals("TestDesc", workspace.getDescription()),
+                () -> Assertions.assertEquals(WorkspaceVisibility.PUBLIC, workspace.getVisibility())
         );
     }
 
@@ -33,6 +36,13 @@ class WorkspaceServiceTest extends BaseTest {
 
     @Test
     void update() {
+        Workspace workspace = service.create("user", "Testname", "TestDesc", WorkspaceVisibility.PUBLIC);
+        workspace.setName("Updated");
+        workspace.setUpdatedDate(LocalDateTime.now());
+        service.update(workspace.getId(), workspace);
+        Workspace updated = service.findByID(workspace.getId());
+        Assertions.assertEquals(updated, updated.getName()
+        );
     }
 
     @Test
@@ -41,20 +51,31 @@ class WorkspaceServiceTest extends BaseTest {
 
     @Test
     void print() {
+        Workspace workspace = service.create("user", "Testname", "TestDesc", WorkspaceVisibility.PUBLIC);
+        Assertions.assertEquals(workspace.toString(), workspace.toString());
     }
 
     @Test
     void findByID() {
-        Workspace workspace = service.create("user", "Testname" , "TestDesc", WorkspaceVisibility.PRIVATE);
+        Workspace workspace = service.create("user", "Testname", "TestDesc", WorkspaceVisibility.PUBLIC);
         Workspace workspace1 = service.findByID(workspace.getId());
-        Assertions.assertAll(()->Assertions.assertEquals("user", workspace1.getCreatedBy()),
-                ()->Assertions.assertEquals("Testname", workspace1.getName()),
-                ()->Assertions.assertEquals("TestDesc", workspace1.getDescription()),
-                ()->Assertions.assertEquals(WorkspaceVisibility.PRIVATE, workspace1.getVisibility())
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("user", workspace1.getCreatedBy()),
+                () -> Assertions.assertEquals("Testname", workspace1.getName()),
+                () -> Assertions.assertEquals("TestDesc", workspace1.getDescription()),
+                () -> Assertions.assertEquals(WorkspaceVisibility.PUBLIC, workspace1.getVisibility())
         );
     }
 
     @Test
     void deleteByID() {
+        Workspace workspace = service.create("user", "Testname", "TestDesc", WorkspaceVisibility.PUBLIC);
+        Workspace workspace1 = service.create("user", "Testname", "TestDesc", WorkspaceVisibility.PUBLIC);
+        Boolean delete = service.deleteByID(workspace.getId());
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("user", workspace1.getCreatedBy()),
+                () -> Assertions.assertEquals(null, workspace.getId())
+        );
     }
 }
